3d4b9f712ba6e9168c99cd87846c2fd84de12f5e
Nathan Chon <nchon99@gmail.com>
2023-01-30 19:24:21 +0000

    completed JavaScript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 5;
+var height = 6;
+var area = width * height;
+console.log('the value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 15;
+var payment = 20;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 95;
+var final = 93;
+var grade = (quizzes + midterm + final) / 3;
+console.log('valyue of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Nathan';
+var lastName = 'Chon';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 208;
+var isSparata = headCount === 300;
+console.log('value of isSparata:', isSparata);
+console.log('typeof isSparta:', typeof isSparata);
+
+var motto = fullName + ' is the GOAT!';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
