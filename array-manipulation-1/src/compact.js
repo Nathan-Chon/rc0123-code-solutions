2b3c2a524656b8474e6f17141c97b24d7f7f75b3
Nathan Chon <nchon99@gmail.com>
2023-02-03 21:18:32 +0000

    Revise Compact portion of Array Manipulations

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -4,14 +4,14 @@ Define a function, named 'compact', that accepts one arguement, 'array'.
   declare a variable named 'newArray' as an empty array.
   being a loop that iterates through the entire 'array', where
     for each index in the array:
-      Create an if statement that determines if the code contains any of the not allowed values.
+      Create an if statement that determines if the code is truthy.
       Add the values to 'newAray'.
     return the value of newArray.
 */
 function compact(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== null && array[i] !== 0 && array[i] !== undefined && array[i] !== '' && !Number.isNaN(array[i])) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
