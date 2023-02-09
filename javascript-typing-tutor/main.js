4bc51710011abad999149502c8eb445a75c2650a
Nathan Chon <nchon99@gmail.com>
2023-02-09 00:40:16 +0000

    typing tutor inprogress 2

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,6 @@
+var currentIndex = 0;
+var $quote = document.querySelectorAll('span');
+var counter = $quote.childNode(currentIndex);
+document.addEventListener('keydown', function (event) {
+  return counter;
+});
