96b1a1ce1be9377e2af7c8b27d7e1df2882643bd
Nathan Chon <nchon99@gmail.com>
2023-02-11 00:02:59 +0000

    Completed JavaScript Typing Tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,11 +2,22 @@ var currentIndex = 0;
 var $quote = document.querySelectorAll('span');
 document.addEventListener('keydown', function (event) {
   var $tracker = $quote.item(currentIndex);
+  var $underline = $quote.item(currentIndex + 1);
+  $tracker.setAttribute('class', 'underline');
   event.preventDefault();
-  if ($tracker.innerText === event.key) {
+  if ($tracker.innerText === event.key && currentIndex < 28) {
     $tracker.setAttribute('class', 'correct');
+    $underline.setAttribute('class', 'underline');
     currentIndex++;
+  } else if ($tracker.innerText === event.key && currentIndex >= 28) {
+    $tracker.setAttribute('class', 'correct');
+    $play.setAttribute('class', 'play');
   } else {
     $tracker.setAttribute('class', 'incorrect');
   }
+});
+
+var $play = document.querySelector('.play');
+$play.addEventListener('click', function (event) {
+  document.location.reload();
 });
