0776328616ebad87414c8a38b31dbafbad3db535
Nathan Chon <nchon99@gmail.com>
2023-02-20 17:30:03 +0000

    Completed JavaScript Custom Methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,34 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    var count = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      count += numbers[i];
+
+    }
+    return count;
+  },
+  getAverage: function (numbers) {
+    var average = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      average += numbers[i];
+    }
+    return average / numbers.length;
+  }
+};
