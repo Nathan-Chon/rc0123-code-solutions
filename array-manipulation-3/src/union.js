d68a91b1563e2fa9ec7c3181c163fca6fbc2dccc
Nathan Chon <nchon99@gmail.com>
2023-02-24 01:04:21 +0000

    Completed Array Manipulations 3

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,12 @@
 /* exported union */
+function union(first, second) {
+  var array = [];
+  for (var i = 0; i < first.length; i++) {
+    array.push(first[i]);
+  }
+  for (var j = 0; j < second.length; j++) {
+    array.push(second[j]);
+  }
+
+  return array.filter((item, index) => array.indexOf(item) === index);
+}
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,20 @@
 /* exported unique */
+function unique(array) {
+  var newArray = [];
+  var start = false;
+  var count = 0;
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < newArray.length; j++) {
+      if (array[i] === newArray[j]) {
+        start = true;
+      }
+    }
+    count++;
+    if (count === 1 && start === false) {
+      newArray.push(array[i]);
+    }
+    start = false;
+    count = 0;
+  }
+  return newArray;
+}
