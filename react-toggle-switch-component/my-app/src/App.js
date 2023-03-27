7f61aef578bb6476e87e4a317e659b31d5c13ebb
Nathan Chon <nchon99@gmail.com>
2023-03-27 16:56:43 +0000

    Completed Toggle Swtich

---
+++ react-toggle-switch-component/my-app/src/App.js
@@ -0,0 +1,16 @@
+import ToggleSwitch from './ToggleSwitch';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div><ToggleSwitch ></ToggleSwitch></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
