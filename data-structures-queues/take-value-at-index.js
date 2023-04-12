28d205f8a4a42610f4f0771665c627d720f2e64d
Nathan Chon <nchon99@gmail.com>
2023-04-12 17:07:59 +0000

    Completed Data Structures queue

---
+++ data-structures-queues/take-value-at-index.js
@@ -0,0 +1,7 @@
+export default function takeValueAtIndex(queue, index) {
+  while (queue.peek() !== undefined && index > 0) {
+    queue.enqueue(queue.dequeue());
+    index--;
+  }
+  return queue.dequeue();
+}
